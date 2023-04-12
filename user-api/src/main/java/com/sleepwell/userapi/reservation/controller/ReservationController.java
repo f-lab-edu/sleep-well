b7a8abc5363faa1106d970138b0a3e61f60e4d59
rio <75190035+HyoJongPark@users.noreply.github.com>
2023-04-12 15:24:24 +0900

    refactor: 와일드카드 import 삭제

--- user-api/src/main/java/com/sleepwell/userapi/reservation/controller/ReservationController.java
+++ user-api/src/main/java/com/sleepwell/userapi/reservation/controller/ReservationController.java
@@ -5,7 +5,13 @@ import com.sleepwell.userapi.reservation.dto.ReservationRequestDto;
 import com.sleepwell.userapi.reservation.service.ReservationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 import java.util.Optional;
