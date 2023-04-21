7118adc682268142e7a04fd1b2e63fd918f5aa2f
rio <75190035+HyoJongPark@users.noreply.github.com>
2023-04-21 13:55:31 +0900

    test: 숙소 상세 정보 조회 테스트

---
+++ user-api/src/test/java/com/sleepwell/userapi/accommodation/service/AccommodationServiceTest.java
@@ -0,0 +1,51 @@
+package com.sleepwell.userapi.accommodation.service;
+
+import com.sleepwell.userapi.accommodation.entity.Accommodation;
+import com.sleepwell.userapi.accommodation.repository.AccommodationRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class AccommodationServiceTest {
+
+    @Mock
+    AccommodationRepository accommodationRepository;
+
+    @InjectMocks
+    AccommodationService accommodationService;
+
+    @DisplayName("존재하지 않는 숙소 조회 시 예외 발생")
+    @Test
+    void findAccommodationWithInvalidAccommodationId() {
+        //given
+        Accommodation mockAccommodation = mock(Accommodation.class);
+        when(accommodationRepository.findById(any())).thenReturn(Optional.empty());
+
+        //then
+        assertThrows(RuntimeException.class, () -> accommodationService.getAccommodation(1L));
+    }
+
+    @DisplayName("유효한 숙소 조회 시 예외 발생")
+    @Test
+    void findAccommodationWithValidAccommodationId() {
+        //given
+        Accommodation mockAccommodation = mock(Accommodation.class);
+        when(accommodationRepository.findById(any())).thenReturn(Optional.of(mockAccommodation));
+
+        //then
+        Accommodation findAccommodation = accommodationService.getAccommodation(1L);
+        assertEquals(mockAccommodation, findAccommodation);
+    }
+}
