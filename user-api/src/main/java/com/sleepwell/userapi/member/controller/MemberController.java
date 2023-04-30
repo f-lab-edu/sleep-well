95417bdc38dff83b312850eea65b00f9546afd7b
rio <75190035+HyoJongPark@users.noreply.github.com>
2023-04-30 17:17:59 +0900

    feat: 비밀번호 암호화 구현

--- user-api/src/main/java/com/sleepwell/userapi/member/controller/MemberController.java
+++ user-api/src/main/java/com/sleepwell/userapi/member/controller/MemberController.java
@@ -7,10 +7,12 @@ import com.sleepwell.userapi.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/member")
 @RequiredArgsConstructor
-@RestController("/member")
 public class MemberController {
 
     private final MemberService memberService;
--- user-api/src/main/java/com/sleepwell/userapi/member/service/MemberService.java
+++ user-api/src/main/java/com/sleepwell/userapi/member/service/MemberService.java
@@ -3,6 +3,7 @@ package com.sleepwell.userapi.member.service;
 import com.sleepwell.userapi.member.entity.Member;
 import com.sleepwell.userapi.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,12 +11,14 @@ import org.springframework.stereotype.Service;
 public class MemberService {
 
     private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public Member createMember(Member member) {
         if (memberRepository.existsByEmail(member.getEmail())) {
             throw new RuntimeException("중복된 이메일입니다. 다시 입력해주세요.");
         }
 
+        member.setPassword(passwordEncoder.encode(member.getPassword()));
         return memberRepository.save(member);
     }
 }
