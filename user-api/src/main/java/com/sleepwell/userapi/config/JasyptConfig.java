1eadde964f93efe825775ad307bd21f54b1b2cbf
rio <75190035+HyoJongPark@users.noreply.github.com>
2023-05-04 15:56:19 +0900

    refactor: @Value -> @ConfigurationProperties 로 교체

--- user-api/src/main/java/com/sleepwell/userapi/config/JasyptConfig.java
+++ user-api/src/main/java/com/sleepwell/userapi/config/JasyptConfig.java
@@ -1,22 +1,24 @@
 package com.sleepwell.userapi.config;
 
+import lombok.Setter;
 import org.jasypt.encryption.StringEncryptor;
 import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
 import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@Setter
 @Configuration
+@ConfigurationProperties("jasypt.encryptor")
 public class JasyptConfig {
 
-    @Value("${jasypt.encryptor.password}")
-    private String encryptKey;
+    private String password;
 
     @Bean
     public StringEncryptor jasyptStringEncryptor() {
         SimpleStringPBEConfig config = new SimpleStringPBEConfig();
-        config.setPassword(encryptKey);
+        config.setPassword(password);
         config.setAlgorithm("PBEWithMD5AndDES");
         config.setKeyObtentionIterations("1000");
         config.setPoolSize("1");
--- user-api/src/main/java/com/sleepwell/userapi/config/PaymentConfig.java
+++ user-api/src/main/java/com/sleepwell/userapi/config/PaymentConfig.java
@@ -1,17 +1,18 @@
 package com.sleepwell.userapi.config;
 
 import com.siot.IamportRestClient.IamportClient;
-import org.springframework.beans.factory.annotation.Value;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@Setter
 @Configuration
+@ConfigurationProperties("iamport")
 public class PaymentConfig {
 
-    @Value("${iamport.rest.api.key}")
     private String apiKey;
 
-    @Value("${iamport.rest.api.secret}")
     private String apiSecret;
 
     @Bean
