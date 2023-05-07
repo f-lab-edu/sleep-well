d78a1ec9b6f1d1b435cd182ce92dc9d1f87eca61
rio <75190035+HyoJongPark@users.noreply.github.com>
2023-05-07 11:45:38 +0900

    feat: JwtFilter 구현 및 등록

---
+++ user-api/src/main/java/com/sleepwell/userapi/util/JwtFilter.java
@@ -0,0 +1,51 @@
+package com.sleepwell.userapi.util;
+
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Setter
+@Component
+@ConfigurationProperties("jwt.filter")
+public class JwtFilter extends OncePerRequestFilter {
+
+    private String header;
+    private String prefix;
+    private final JwtProvider jwtProvider;
+
+    public JwtFilter(JwtProvider jwtProvider) {
+        this.jwtProvider = jwtProvider;
+    }
+
+    @Override
+    public void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+        String jwt = resolveToken(request);
+        // 토큰이 정상이면 Authentication 을 가져와서 SecurityContext 에 저장
+        if (StringUtils.hasText(jwt) && jwtProvider.validateToken(jwt)) {
+            Authentication authentication = jwtProvider.getAuthentication(jwt);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        chain.doFilter(request, response);
+    }
+
+    private String resolveToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader(header);
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(prefix)) {
+            return bearerToken.substring(7);
+        }
+
+        return null;
+    }
+
+}
