98fda86a7a486831062e55f84275fcfcd4deec7d
rio <75190035+HyoJongPark@users.noreply.github.com>
2023-05-07 14:05:34 +0900

    feat: UserDetailsService 구현

--- user-api/src/main/java/com/sleepwell/userapi/member/repository/MemberRepository.java
+++ user-api/src/main/java/com/sleepwell/userapi/member/repository/MemberRepository.java
@@ -3,19 +3,25 @@ package com.sleepwell.userapi.member.repository;
 import com.sleepwell.userapi.member.entity.Member;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public class MemberRepository {
-
-    public Member findById(Long guestId) {
-        return new Member("이름", "eamil@email.com", "password");
-    }
-
-    public boolean existsByEmail(String email) {
-        return false;
-    }
 
     public Member save(Member member) {
         member.setId(1L);
         return member;
+    }
+
+    public Member findById(Long guestId) {
+        return new Member("이름", "eamil@email.com", "{bcrypt}$2a$10$N5t/NQnCqg9ONzUrJTa06Ol7jEBOZDi07ZLlQMFJLCfo6uHs4Fjr.");
+    }
+
+    public Optional<Member> findByEmail(String email) {
+        return Optional.of(new Member("이름", "eamil@email.com", "{bcrypt}$2a$10$N5t/NQnCqg9ONzUrJTa06Ol7jEBOZDi07ZLlQMFJLCfo6uHs4Fjr."));
+    }
+
+    public boolean existsByEmail(String email) {
+        return false;
     }
 }
