5f73b904a69ce5fcdb1619cfe2e9dee0a0e80497
rio <75190035+HyoJongPark@users.noreply.github.com>
2023-05-18 08:23:55 +0900

    refactor: 기존 Repository 를 JPA Repository 로 변경

--- user-api/src/main/java/com/sleepwell/userapi/accommodation/repository/AccommodationRepository.java
+++ user-api/src/main/java/com/sleepwell/userapi/accommodation/repository/AccommodationRepository.java
@@ -2,6 +2,8 @@ package com.sleepwell.userapi.accommodation.repository;
 
 import com.sleepwell.userapi.accommodation.dto.AccommodationSearchDto;
 import com.sleepwell.userapi.accommodation.entity.Accommodation;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
@@ -10,14 +12,11 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public class AccommodationRepository {
+public interface AccommodationRepository extends JpaRepository<Accommodation, Long> {
 
-    public Optional<Accommodation> findById(Long accommodationId) {
-        return Optional.of(new Accommodation("숙소 이름", 1_000_000, "숙소 타입", "지역", LocalDate.of(2023, 4, 24), LocalDate.of(2023, 4, 25), LocalTime.of(15, 0), LocalTime.of(11, 0), 10, "상세 정보"));
-    }
+    public Optional<Accommodation> findById(@NotNull Long accommodationId);
 
     //동적쿼리 사용 예정
-    public List<Accommodation> findAllByAccommodationSearchDto(AccommodationSearchDto accommodationSearchDto) {
-        return List.of(new Accommodation("숙소 이름", 1_000_000, "숙소 타입", "지역", LocalDate.of(2023, 4, 24), LocalDate.of(2023, 4, 25), LocalTime.of(15, 0), LocalTime.of(11, 0), 10, "상세 정보"));
-    }
+    public List<Accommodation> findAllByAccommodationSearchDto(AccommodationSearchDto accommodationSearchDto);
+
 }
--- user-api/src/main/java/com/sleepwell/userapi/member/repository/MemberRepository.java
+++ user-api/src/main/java/com/sleepwell/userapi/member/repository/MemberRepository.java
@@ -1,27 +1,19 @@
 package com.sleepwell.userapi.member.repository;
 
 import com.sleepwell.userapi.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public class MemberRepository {
+public interface MemberRepository extends JpaRepository<Member, Long> {
 
-    public Member save(Member member) {
-        member.setId(1L);
-        return member;
-    }
+    public Member save(Member member);
 
-    public Member findById(Long guestId) {
-        return new Member("이름", "eamil@email.com", "{bcrypt}$2a$10$N5t/NQnCqg9ONzUrJTa06Ol7jEBOZDi07ZLlQMFJLCfo6uHs4Fjr.");
-    }
+    public Optional<Member> findById(Long guestId);
 
-    public Optional<Member> findByEmail(String email) {
-        return Optional.of(new Member("이름", "eamil@email.com", "{bcrypt}$2a$10$N5t/NQnCqg9ONzUrJTa06Ol7jEBOZDi07ZLlQMFJLCfo6uHs4Fjr."));
-    }
+    public Optional<Member> findByEmail(String email);
 
-    public boolean existsByEmail(String email) {
-        return false;
-    }
+    public boolean existsByEmail(String email);
 }
