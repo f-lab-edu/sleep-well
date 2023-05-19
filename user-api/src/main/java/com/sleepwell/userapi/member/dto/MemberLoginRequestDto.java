641d7ead95f7442ae96d5b058beaba3451ca23f4
rio <75190035+HyoJongPark@users.noreply.github.com>
2023-05-19 09:34:06 +0900

    refactor: pr 분리로 인한 불필요 변경 삭제

--- user-api/src/main/java/com/sleepwell/userapi/member/dto/MemberLoginRequestDto.java
+++ user-api/src/main/java/com/sleepwell/userapi/member/dto/MemberLoginRequestDto.java
@@ -3,16 +3,12 @@ package com.sleepwell.userapi.member.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-import javax.validation.constraints.NotBlank;
-
 @Getter
 @AllArgsConstructor
 public class MemberLoginRequestDto {
 
-    @NotBlank
     private final String email;
 
-    @NotBlank
     private final String password;
 
 }
