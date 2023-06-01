e228e4dce8c5fef71d6efe509e54b5b6793ae8fc
rio <75190035+HyoJongPark@users.noreply.github.com>
2023-06-01 12:07:00 +0900

    refactor: 예약 일자 사이 이미 예약된 숙소를 찾아내는 로직 수정

--- user-api/src/main/java/com/sleepwell/userapi/reservation/repository/ReservationRepository.java
+++ user-api/src/main/java/com/sleepwell/userapi/reservation/repository/ReservationRepository.java
@@ -3,6 +3,7 @@ package com.sleepwell.userapi.reservation.repository;
 import com.sleepwell.userapi.reservation.entity.Reservation;
 import com.sleepwell.userapi.reservation.entity.ReservationStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
@@ -11,7 +12,12 @@ import java.util.List;
 @Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
 
-    boolean existsByAccommodationIdAndCheckInDateGreaterThanEqualAndCheckOutDateLessThanEqual(Long accommodationId, LocalDate checkInDate, LocalDate checkOutDate);
+    @Query("select count(r) > 0 " +
+            "from Reservation r " +
+            "where r.accommodation.id = :accommodationId " +
+            "and (r.checkInDate between :checkInDate and :checkOutDate " +
+            "or r.checkOutDate between :checkInDate and :checkOutDate)")
+    boolean existsReservationInAccommodationThatDay(Long accommodationId, LocalDate checkInDate, LocalDate checkOutDate);
 
     List<Reservation> findByReservationStatusAndReservedDateLessThanEqual(ReservationStatus reservationStatus, LocalDate localDate);
 
