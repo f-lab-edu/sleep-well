6e3c9858f19fd58a5db1bf50cd17b34c231fd975
rio <75190035+HyoJongPark@users.noreply.github.com>
2023-06-05 17:23:54 +0900

    refactor: 사용되지 않는 메소드 삭제

--- user-api/src/main/java/com/sleepwell/userapi/reservation/repository/ReservationRepository.java
+++ user-api/src/main/java/com/sleepwell/userapi/reservation/repository/ReservationRepository.java
@@ -1,18 +1,14 @@
 package com.sleepwell.userapi.reservation.repository;
 
 import com.sleepwell.userapi.reservation.entity.Reservation;
-import com.sleepwell.userapi.reservation.entity.ReservationStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
-import java.util.List;
 
 @Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
 
     boolean existsByAccommodationIdAndCheckInDateGreaterThanEqualAndCheckOutDateLessThanEqual(Long accommodationId, LocalDate checkInDate, LocalDate checkOutDate);
-
-    List<Reservation> findByReservationStatusAndReservedDateLessThanEqual(ReservationStatus reservationStatus, LocalDate localDate);
 
 }
