dfeeb0cc5376575d25f76a29419905bc46bac647
rio <75190035+HyoJongPark@users.noreply.github.com>
2023-06-08 10:29:35 +0900

    refactor: 클래스 변수 접근 제어자 명시

--- user-api/src/integrationtest/java/com/sleepwell/userapi/member/MemberRepositoryTest.java
+++ user-api/src/integrationtest/java/com/sleepwell/userapi/member/MemberRepositoryTest.java
@@ -24,7 +24,7 @@ public class MemberRepositoryTest {
     @Autowired
     private MemberRepository memberRepository;
 
-    Member member;
+    private Member member;
 
     @BeforeEach
     void setup() {
