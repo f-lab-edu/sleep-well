b701b97f64fa5fc92ceb7ee48861ddb1d4c8a682
rio <75190035+HyoJongPark@users.noreply.github.com>
2023-06-22 10:24:14 +0900

    feat: logging에 kafka 적용

--- user-api/src/main/java/com/sleepwell/userapi/error/handler/GlobalExceptionHandler.java
+++ user-api/src/main/java/com/sleepwell/userapi/error/handler/GlobalExceptionHandler.java
@@ -2,25 +2,32 @@ package com.sleepwell.userapi.error.handler;
 
 import com.sleepwell.userapi.error.ErrorStatus;
 import com.sleepwell.userapi.error.exception.BaseException;
+import com.sleepwell.userapi.log.LogMessage;
+import com.sleepwell.userapi.log.kafka.LogProducer;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.logging.LogLevel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
+@RequiredArgsConstructor
 @RestControllerAdvice(annotations = RestController.class)
 public class GlobalExceptionHandler {
 
+    private final LogProducer logProducer;
+
     @ExceptionHandler(BaseException.class)
     public ResponseEntity<String> baseExceptionHandle(BaseException e) {
-        log.error("{} - {}", e.getMessage(), e.getStackTrace());
+        logProducer.send(new LogMessage(String.format("%s - %s", e.getMessage(), e.getStackTrace()), LogLevel.DEBUG));
         return ResponseEntity.status(e.getStatusCode()).body(e.getMessage());
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> unexpectedExceptionHandle(Exception e) {
-        log.error("{} - {}", e.getMessage(), e.getStackTrace());
+        logProducer.send(new LogMessage(String.format("%s - %s", e.getMessage(), e.getStackTrace()), LogLevel.ERROR));
         return ResponseEntity.status(ErrorStatus.UNEXPECTED_EXCEPTION.getStatusCode()).body(ErrorStatus.UNEXPECTED_EXCEPTION.getMessage());
     }
 }
