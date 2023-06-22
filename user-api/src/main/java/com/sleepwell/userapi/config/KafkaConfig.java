eb100a681d6ff711645404addc7451a758dc2226
rio <75190035+HyoJongPark@users.noreply.github.com>
2023-06-22 10:17:41 +0900

    feat: kafka Admin 설정

---
+++ user-api/src/main/java/com/sleepwell/userapi/config/KafkaConfig.java
@@ -0,0 +1,31 @@
+package com.sleepwell.userapi.config;
+
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaAdmin;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@EnableKafka
+@Configuration
+public class KafkaConfig {
+
+    @Bean
+    public KafkaAdmin kafkaAdmin() {
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9099");
+        return new KafkaAdmin(configs);
+    }
+
+    @Bean
+    public NewTopic logTopic() {
+        return TopicBuilder
+                .name("log-topic")
+                .build();
+    }
+}
