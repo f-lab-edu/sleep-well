556323c470a88eaca932427bb9eb494df3d2b2a2
rio <75190035+HyoJongPark@users.noreply.github.com>
2023-06-22 10:18:37 +0900

    feat: kafka Producer 구현

---
+++ user-api/src/main/java/com/sleepwell/userapi/log/kafka/LogProducer.java
@@ -0,0 +1,19 @@
+package com.sleepwell.userapi.log.kafka;
+
+import com.sleepwell.userapi.log.LogMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class LogProducer {
+
+    private final KafkaTemplate<String, LogMessage> logMessageKafkaTemplate;
+    
+    private static final String TOPIC_NAME = "log-topic";
+
+    public void send(LogMessage message) {
+        logMessageKafkaTemplate.send(TOPIC_NAME, message);
+    }
+}
